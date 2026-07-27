2d259782522440a2ba5ac95548fe84a02ad6806f
Sreenandana <sreenandanapanangattu16@gmail.com>
2026-07-27 09:57:45 +0530

    feat-version 1

--- components/layout/DesktopLayout.tsx
+++ components/layout/DesktopLayout.tsx
@@ -4,7 +4,7 @@ import { TerminalBar } from "@/components/layout/TerminalBar";
 export function DesktopLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="scanlines flex min-h-screen flex-col md:flex-row">
-      <Sidebar />
+      <Sidebar isOpen={true} onClose={() => {}} />
       <div className="flex min-h-screen flex-1 flex-col md:ml-0">
         <main id="main-content" className="flex-1 px-4 py-16 md:px-8 md:py-8">
           {children}
